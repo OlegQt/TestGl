ca57612470b024d86db3ba06fc04b8db46a7e3dd
Magister <xqjh@mail.ru>
2024-10-05 20:22:05 +0300

    Timer implemented from starting App timer displayed on SupportActionBar subtitle

--- app/src/main/java/com/testgl/RootActivity.kt
+++ app/src/main/java/com/testgl/RootActivity.kt
@@ -1,25 +1,68 @@
 package com.testgl
 
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.testgl.databinding.ActivityRootBinding
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityRootBinding
+    private lateinit var binding: ActivityRootBinding
+    private var globalCount: Int = 100
+    private var countingEnabled = true
+    private var countJob: Job? = null
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val bar = supportActionBar
-        bar?.title = "Title"
-        bar?.subtitle = "Test"
 
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.txt.text = "Hello"
+        setUpUi()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        countJob?.cancel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startTimer()
+    }
+
+    private fun setUpUi() {
+        supportActionBar?.apply {
+            title = "Testing application"
+            subtitle = "timer"
+
+            show()
+        }
+
+        startTimer()
+    }
+
+    private fun startTimer() {
+        val exceptionHandler =
+            CoroutineExceptionHandler { coroutineContext: CoroutineContext, throwable: Throwable ->
+                binding.txt.text = throwable.message.toString()
+            }
+        countJob = MainScope().launch(exceptionHandler) {
+            while (true) {
+                if (countingEnabled) {
+                    delay(10)
+                    globalCount++
+                    supportActionBar?.subtitle = globalCount.toString()
+                }
+            }
+        }
     }
 }
