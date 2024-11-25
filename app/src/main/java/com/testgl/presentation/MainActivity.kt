a7e3f1fab3487d8202136a802d880775dd159047
Magister <xqjh@mail.ru>
2024-11-25 17:26:12 +0300

    Binding implementation

--- app/src/main/java/com/testgl/presentation/MainActivity.kt
+++ app/src/main/java/com/testgl/presentation/MainActivity.kt
@@ -3,11 +3,25 @@ package com.testgl.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.testgl.R
+import com.testgl.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding:ActivityMainBinding?= null
+    private val binding get() = _binding ?: throw Exception("null binding NPE")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
 
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setUpUi()
+    }
+
+    private fun setUpUi(){
+        supportActionBar?.let {
+            it.title = "GUI test"
+            it.subtitle = "application"
+        }
     }
 }
