c955f6da53212f806231cdff3eb94e2f7e608a4a
Magister <xqjh@mail.ru>
2024-11-27 23:29:49 +0300

    Создан стиль для bottomNavBar Для выделения иконок создан селектор цвета

--- app/src/main/java/com/testgl/presentation/MainActivity.kt
+++ app/src/main/java/com/testgl/presentation/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.testgl.presentation
 
 import android.os.Bundle
-import android.widget.FrameLayout
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.google.android.material.snackbar.Snackbar
+import com.testgl.R
 import com.testgl.databinding.ActivityMainBinding
 import com.testgl.presentation.fragments.RootFragment
 
@@ -12,7 +17,9 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding ?: throw Exception("null binding NPE")
 
-    private var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>? = null
+    private var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>? = null
+
+    private var countPush = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +30,20 @@ class MainActivity : AppCompatActivity() {
         setUpUi()
     }
 
+    private fun expandBottomSheet(): Boolean {
+        bottomSheetBehavior?.let {
+            it.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+            return true
+        }
+        return false
+    }
+
+    private fun showSnack(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE)
+            .setAction("ok") { }
+            .show()
+    }
+
     private fun setUpUi() {
         supportActionBar?.let {
             it.title = "GUI test"
@@ -30,16 +51,49 @@ class MainActivity : AppCompatActivity() {
         }
 
         supportFragmentManager.commit {
-            replace(binding.rootFragmentHolder.id, RootFragment() )
+            replace(binding.rootFragmentHolder.id, RootFragment())
         }
 
         bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheetLayout).also {
             it.peekHeight = 200
             it.isHideable = true
-            binding.bottomSheetLayout.alpha = 0.5f
-            it.state =BottomSheetBehavior.STATE_HIDDEN
+            binding.bottomSheetLayout.alpha = 0.8f
+            binding.bottomSheetLayout.isClickable = true
+            it.state = BottomSheetBehavior.STATE_HIDDEN
+
+            it.addBottomSheetCallback(object : BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    when (newState) {
+                        BottomSheetBehavior.STATE_HIDDEN -> showSnack("STATE_HIDDEN")
+                        BottomSheetBehavior.STATE_COLLAPSED -> showSnack("STATE_COLLAPSED")
+                        BottomSheetBehavior.STATE_EXPANDED -> showSnack("STATE_EXPANDED")
+                        BottomSheetBehavior.STATE_DRAGGING -> showSnack("STATE_DRAGGING")
+                        BottomSheetBehavior.STATE_HALF_EXPANDED -> showSnack("STATE_DRAGGING")
+                        else -> {}
+                    }
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    binding.sliderOffset.text = slideOffset.toString()
+                }
+            })
         }
 
+        binding.rootNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.root_page -> {
+                    countPush++
+                    Toast.makeText(this, countPush.toString(), Toast.LENGTH_SHORT).show()
+                }
 
+                R.id.info_page -> {
+                    countPush++
+                    Toast.makeText(this, countPush.toString(), Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.console_page -> expandBottomSheet()
+            }
+            true
+        }
     }
 }
--- app/src/main/java/com/testgl/presentation/fragments/RootFragment.kt
+++ app/src/main/java/com/testgl/presentation/fragments/RootFragment.kt
@@ -5,15 +5,6 @@ import com.testgl.R
 
 
 class RootFragment : Fragment(R.layout.fragment_root) {
-
-/*    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_root, container, false)
-    }*/
-
     companion object {
 
     }
