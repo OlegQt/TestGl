dc41d0d88745f4dda8cb02139e563e7513d9fb83
Magister <xqjh@mail.ru>
2024-12-30 09:10:05 +0300

    Оптимизация класса bytePresenter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.testgl"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.testgl"
         minSdk = 22
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -36,10 +36,16 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    // Kotlin
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    // LifeCycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/testgl/presentation/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.testgl.presentation.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.testgl.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
