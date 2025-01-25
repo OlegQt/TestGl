0d6bfcebd11586c9efef21c4f2f60c00cce56e97
Magister <xqjh@mail.ru>
2025-01-25 11:41:59 +0300

    На экране ScrambleGameScreen реализован текстовый ввод Условия тестового ввода пользователем: Разрешены к вводу только символы искомой строки (scrambleWord) При вводе разрешенный символов, из исходной строки они исчезают с анимацией

--- app/src/main/java/com/testgl/presentation/screens/ScrambleGameScreen.kt
+++ app/src/main/java/com/testgl/presentation/screens/ScrambleGameScreen.kt
@@ -1,38 +1,36 @@
 package com.testgl.presentation.screens
 
-import androidx.compose.animation.AnimatedContent
+import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.testgl.presentation.theme.AppTheme
 import com.testgl.presentation.viewmodels.ScrambleGameViewModel
-import kotlinx.coroutines.Dispatchers
 
 @Composable
 fun ScrambleGameScreen(
@@ -41,52 +39,82 @@ fun ScrambleGameScreen(
 ) {
     val uiState = viewModel.uiState.collectAsState()
 
+
     Box(
         modifier = modifier
             .fillMaxSize()
-            .background(Color.DarkGray),
+            .background(Color.Transparent),
         contentAlignment = Alignment.Center
 
     ) {
-        Column {
-            Text(
-                //modifier = Modifier.align(Alignment.Center),
-                text = uiState.value.currentScrambleWord,
-                textAlign = TextAlign.Center,
-                fontSize = 32.sp
-            )
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            val sourceWord by rememberSaveable { mutableStateOf("Window") }
 
-            IconButton(
-                onClick = { viewModel.pickNewWord() },
-                colors = IconButtonDefaults.iconButtonColors(contentColor = Color.Black)
+            // На основе сравнения с этой строкой будут загружаться анимации видимости букв
+            var processedWord by remember { mutableStateOf(sourceWord) }
 
-            ) {
-                Icon(imageVector = Icons.Filled.AddCircle,contentDescription = null)
+            var inputString by rememberSaveable { mutableStateOf("") }
+
+            Row {
+                sourceWord.toCharArray().forEachIndexed { index, symbol ->
+
+                    //Анализируем видимость отдельных букв
+                    var isVisible by mutableStateOf(sourceWord[index] == processedWord[index])
+
+                    AnimatedVisibility(isVisible) {
+                        Card(
+                            Modifier
+                                .padding(horizontal = 1.dp)
+                                .clickable { isVisible = false })
+                        {
+                            Text(symbol.toString(), fontSize = 36.sp)
+                        }
+                    }
+                }
             }
 
             OutlinedTextField(
-                value = "Text",
-                shape = RoundedCornerShape(percent = 10),
-                colors = OutlinedTextFieldDefaults.colors(unfocusedContainerColor = Color.LightGray),
-                onValueChange = {}
+                modifier = Modifier.padding(top = 16.dp),
+                value = inputString,
+                onValueChange = { newInput ->
+
+                    val filterResult = filterInputText(
+                        inputTxtVal = newInput,
+                        sourceFilterTxt = sourceWord
+                    )
+
+                    inputString = filterResult.first
+                    processedWord = filterResult.second
+                },
+                shape = RoundedCornerShape(percent = 50),
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedContainerColor = MaterialTheme.colorScheme.onPrimary
+                ),
             )
-            Row(modifier = Modifier.padding(top = 24.dp)) {
-
-
-                val word = uiState.value.currentScrambleWord
-                word.toCharArray().forEach {
-                    val visibility by rememberSaveable { mutableStateOf(true) }
-
-                    Text(it.toString(), fontSize = 32.sp)
-                }
-            }
         }
-
     }
 }
 
-@Preview(showSystemUi = true)
+fun filterInputText(inputTxtVal: String, sourceFilterTxt: String): Pair<String, String> {
+    var sourceTxt = sourceFilterTxt
+    val finalStr: StringBuilder = StringBuilder()
+
+    inputTxtVal.toCharArray().forEach {
+        if (sourceTxt.contains(it)) {
+            finalStr.append(it)
+            sourceTxt = sourceTxt.replaceFirst(oldChar = it, newChar = Char(126), ignoreCase = true)
+        }
+    }
+
+    //Log.d("LOG"," source = $sourceTxt \n input = $inputTxtVal \n final = $finalStr")
+
+    return Pair(first = String(finalStr), second = sourceTxt)
+}
+
+@Preview(showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun PreviewScrambleGameUi(){
-    ScrambleGameScreen()
+fun PreviewScrambleGameUi() {
+    AppTheme {
+        ScrambleGameScreen()
+    }
 }
