4d4e833ea7524b6f3fb90fe76ec6810f21140c8c
Magister <xqjh@mail.ru>
2025-02-01 11:12:05 +0300

    Обновление кода экрана ScrambleGameScreen

--- app/src/main/java/com/testgl/presentation/screens/ScrambleGameScreen.kt
+++ app/src/main/java/com/testgl/presentation/screens/ScrambleGameScreen.kt
@@ -1,30 +1,29 @@
 package com.testgl.presentation.screens
 
 import android.content.res.Configuration
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,74 +39,76 @@ fun ScrambleGameScreen(
 ) {
     val uiState = viewModel.uiState.collectAsState()
 
-    Space(modifier = modifier)
+    Space(modifier = modifier.fillMaxSize())
 
-    Box(
+    GameCard(
+        modifier = Modifier.fillMaxWidth(),
+        sourceWord = uiState.value.currentScrambleWord
+    )
+}
+
+
+@Composable
+fun GameCard(modifier: Modifier = Modifier, sourceWord: String) {
+    var inputString by rememberSaveable { mutableStateOf("") }
+    var processedWord by remember { mutableStateOf(sourceWord) }
+    var fallingHeight by remember { mutableIntStateOf(0) }
+
+    Card(
         modifier = modifier
-            .fillMaxSize()
-            .background(Color.Transparent),
-        contentAlignment = Alignment.Center
-
+            .padding(top = 128.dp, start = 16.dp, end = 16.dp)
+            .onGloballyPositioned { coordinates ->
+                fallingHeight = coordinates.size.height
+            }
+            .height(256.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onTertiaryContainer.copy(
+                alpha = 0.05f
+            )
+        )
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            val sourceWord by rememberSaveable { mutableStateOf(uiState.value.currentScrambleWord) }
-
-            // На основе сравнения с этой строкой будут загружаться анимации видимости букв
-            var processedWord by remember { mutableStateOf(sourceWord) }
-
-            var inputString by rememberSaveable { mutableStateOf("") }
-
-            Row {
-                sourceWord.toCharArray().forEachIndexed { index, symbol ->
-
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(modifier = Modifier.padding(bottom = 24.dp)) {
+                sourceWord.forEachIndexed { charIndex, charSymbol ->
                     //Анализируем видимость отдельных букв
-                    var isVisible by mutableStateOf(sourceWord[index] == processedWord[index])
+                    val isVisible by mutableStateOf(sourceWord[charIndex] == processedWord[charIndex])
 
-                    AnimatedVisibility(isVisible) {
-                        Card(
-                            modifier = Modifier
-                                .padding(horizontal = 1.dp)
-                                .clickable { isVisible = false },
-                            colors = CardDefaults.cardColors(
-                                containerColor = MaterialTheme.colorScheme.onPrimary.copy(
-                                    alpha = 0.2f
-                                )
-                            )
-                        )
-
-                        {
-                            Text(
-                                modifier = Modifier.padding(all = 2.dp),
-                                text = symbol.toString(),
-                                fontSize = 36.sp,
-                            )
-                        }
-                    }
+                    FallingLetter(
+                        symbol = charSymbol,
+                        charIndex = charIndex,
+                        visibility = isVisible,
+                        rowHeight = fallingHeight
+                    )
                 }
             }
 
-            Box(modifier = Modifier.padding(top = 16.dp)) {
-                OutlinedTextField(
-                    value = inputString,
-                    onValueChange = { newInput ->
-
-                        val filterResult = filterInputText(
-                            inputTxtVal = newInput,
-                            sourceFilterTxt = sourceWord
-                        )
-
-                        inputString = filterResult.first
-                        processedWord = filterResult.second
-                    },
-                    shape = RoundedCornerShape(percent = 50),
-                    textStyle = TextStyle.Default.copy(fontSize = 28.sp)
-                    //colors = OutlinedTextFieldDefaults.colors(                        unfocusedContainerColor = MaterialTheme.colorScheme.onPrimary
-
-                )
-            }
+            UserInput(
+                textLine = inputString,
+                userInputFun = { txt ->
+                    val filterResult =
+                        filterInputText(inputTxtVal = txt, sourceFilterTxt = sourceWord)
+                    processedWord = filterResult.second
+                    inputString = filterResult.first
+                }
+            )
         }
     }
 }
+
+@Composable
+fun UserInput(textLine: String, userInputFun: (String) -> Unit = {}) {
+    OutlinedTextField(
+        value = textLine,
+        onValueChange = { newInput -> userInputFun(newInput) },
+        shape = RoundedCornerShape(percent = 50),
+        textStyle = TextStyle.Default.copy(fontSize = 28.sp)
+    )
+}
+
 
 fun filterInputText(inputTxtVal: String, sourceFilterTxt: String): Pair<String, String> {
     var sourceTxt = sourceFilterTxt
